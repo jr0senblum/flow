c00639b04fe0fec1a9b98c1d9b2998ebe653c94d
Jim Rosenblum <jrosenblum@carelogistics.com>
2016-08-02 15:57:25 -0400

    no message

--- src/controller/flow_landing_controller.erl
+++ src/controller/flow_landing_controller.erl
@@ -1,6 +1,3 @@
-
-
-
 -module(flow_landing_controller, [Req]).
 -compile(export_all).
 
@@ -10,4 +7,18 @@ landing('GET', []) ->
     Flows = boss_db:find(flow,[],[{order_by,name}]),
     {ok, [{flows, Flows}]}.
 
-
+edit('POST',[]) ->
+    Id = binary_to_list(Req:post_param(<<"id">>)),
+    Name = binary_to_list(Req:post_param(<<"name">>)),
+    Description = binary_to_list(Req:post_param(<<"description">>)),
+    case Id of
+        "new" -> 
+            F = flow:new(id, Name, Description, ""),
+            F:save(),
+            {201, "create", []};
+        _Flow_id ->
+            Old = boss_db:find(Id),
+            New = Old:set([{name, Name},{description, Description}]),
+            New:save(),
+            {205, "reset content", []}
+    end.
